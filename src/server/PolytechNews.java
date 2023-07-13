df41718d2a86001b2133382bfc7ae5207833d76c
Seynabou Diop <seynaboudiop@esp.sn>
2023-07-13 00:30:36 +0000

    web service

---
+++ src/server/PolytechNews.java
@@ -0,0 +1,13 @@
+package server;
+
+import service.UserManager;
+
+import javax.xml.ws.Endpoint;
+
+public class PolytechNews {
+    public static void main(String[] args) {
+        String url = "http://localhost:9008/soap";
+        Endpoint.publish(url, new UserManager());
+        System.out.println("Service web SOAP disponible à l'adresse : " + url);
+    }
+}
